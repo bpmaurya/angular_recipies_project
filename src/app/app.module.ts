1bc74b1e36608ff9cb805fc71fff1ccf4c7b9e89
bpmaurya <realvedprakash@gmail.com>
2021-06-13 17:44:49 +0530

    njn

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header/header.component';
+import { HeaderDirective } from './header/header.directive';
+import { RecipiesComponent } from './recipies/recipies.component';
+import { RecipieDetailComponent } from './recipies/recipie-detail/recipie-detail.component';
+import { RecipieListComponent } from './recipies/recipie-list/recipie-list.component';
+import { RecipieItemComponent } from './recipies/recipie-list/recipie-item/recipie-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HeaderDirective,
+    RecipiesComponent,
+    RecipieDetailComponent,
+    RecipieListComponent,
+    RecipieItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
+
+
   ],
   imports: [
     BrowserModule,
